74ff08bb858e9ba075c7bcf6f7835d9b9e96906f
Ted <ted@redpulse.co.ke>
2018-06-08 11:24:18 +0300

    added click listeners in the interface

---
+++ app/src/main/java/com/chat/app/wovio/Interface/ItemClickListener.java
@@ -0,0 +1,12 @@
+package com.chat.app.wovio.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Hp on 6/8/2018.
+ */
+
+public interface ItemClickListener {
+void onClick(View view, int position, boolean isLongPressed);
+
+}
